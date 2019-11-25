f158a7b9b1d4b79d02a2538dafbf924aaabf00be
asimonv <aasimon@uc.cl>
2019-11-25 18:47:48 -0300

    added language categories option

--- routes/options.js
+++ routes/options.js
@@ -2,13 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/text_categories", (req, res, next) => {
-  const categories = [
-    "abortion",
-    "Brexit",
-    "Britain 🇬🇧",
-    "Theresa May",
-    "pineapple pizza 🍍🍕"
-  ];
+  const {
+    query: { language }
+  } = req;
+  const categories =
+    language === "en"
+      ? [
+          "abortion",
+          "Brexit",
+          "Britain 🇬🇧",
+          "Theresa May",
+          "pineapple pizza 🍍🍕"
+        ]
+      : [
+          "Sebastián Piñera",
+          "Michelle Bachelet",
+          "Arturo Vidal",
+          "Alexis Sánchez",
+          "Augusto Pinochet",
+          "Movimiento Feminista",
+          "Aborto",
+          "Inmigración",
+          "Delincuencia",
+          "Karol Dance",
+          "Pensiones",
+          "Desigualdades",
+          "Sequía",
+          "Latinoamérica"
+        ];
   res.json({ data: categories });
 });
 
