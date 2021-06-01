e8ffcd97a8f91a854266df968664ae64028422a1
asimonv <aasimon@uc.cl>
2021-06-01 12:34:32 -0400

    fixed params

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var express = require("express");
-const csv=require('csvtojson')
+const csv = require("csvtojson");
 var router = express.Router();
 
 const fs = require("fs");
@@ -9,7 +9,7 @@ router.get("/", function(req, res, next) {
   res.send("respond with a resource");
 });
 
-router.get("/opinions", async (_, res, _) => {
+router.get("/opinions", async (req, res, next) => {
   const csvFilePath = `${process.cwd()}/public/opinions.csv`;
   const jsonArray = await csv().fromFile(csvFilePath);
   res.json(jsonArray);
