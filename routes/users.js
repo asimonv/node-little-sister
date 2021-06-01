1fff71703f6f8d4a17da54034b1cea8a8feadd67
asimonv <aasimon@uc.cl>
2021-06-01 12:18:33 -0400

    added opinions

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
+const fs = require("fs");
+
 /* GET users listing. */
 router.get("/", function(req, res, next) {
   res.send("respond with a resource");
+});
+
+router.get("/opinions", () => {
+  fs.readFile("../data/opinions.csv", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
 });
 
 module.exports = router;
